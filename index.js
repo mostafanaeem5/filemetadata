85e8445576a015d284ea56ef911894691a5a1b2d
mostafanaeem5 <72983247+mostafanaeem5@users.noreply.github.com>
2022-10-12 13:44:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -8,13 +10,17 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+    const name = req.file.originalname;
+    const type = req.file.mimetype;
+    const size = req.file.size;
+    res.json({ name: name, type: type, size: size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+    console.log('Your app is listening on port ' + port);
 });
